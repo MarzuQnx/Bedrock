a0dad0b78cdb514c4de0d43aefb99d08ad935514
marzuqnx <marzuqnx@gmail.com>
2025-02-18 13:16:22 +0700

    Plugins dan themes config

--- web/app/themes/twentytwentyfive-child/functions.php
+++ web/app/themes/twentytwentyfive-child/functions.php
@@ -6,3 +6,4 @@ function my_twentytwentyfive_child_styles() {
     wp_enqueue_style( 'twentytwentyfive-child-style', get_stylesheet_directory_uri() . '/style.css', array('twentytwentyfive-style'), wp_get_theme()->get('Version') ); // Style child
 }
 add_action( 'wp_enqueue_scripts', 'my_twentytwentyfive_child_styles' );
+
